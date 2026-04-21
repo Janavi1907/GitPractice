7a77a29f5602fb70a1f692c141cfeb7b27a95f52
Janavi S <janavimegu36@gmail.com>
2026-04-21 22:03:53 +0530

    BOX MODEL

--- hello.py
+++ hello.py
@@ -1,3 +1,9 @@
 print("Hello Git")
 print("learning version c")
 print("Consistency builds success")
+student = ('Alice', 20, 'Computer Science', 8.7) 
+name, age, dept, cgpa = student # Tuple unpacking 
+print('Name:', name)
+print('Age:', age) 
+print('Dept:', dept)
+print('CGPA:', cgpa)
