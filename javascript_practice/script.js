3ba4e0f4772d53038b82ebf7b2cfb2f13ca856f3
Janavi S <janavimegu36@gmail.com>
2026-06-19 12:44:31 +0530

    JS prctice

--- javascript_practice/script.js
+++ javascript_practice/script.js
@@ -1,16 +1,74 @@
-let name=prompt("Enter your name:")
+// let name=prompt("Enter your name:")
+// let age=prompt("Enter your age:")
+// let college=prompt("Enter your college name:")
+// console.log("Name:" + name)
+// console.log("Age:" + age)
+// console.log("College:" + college)
+// alert("Name:" + name)
+// alert("Age:" + age)
+// alert("College:" + college)
+// let a=10
+// let b=1
+// console.log("Addition:" + (a + b))
+// console.log("Subtraction:" + (a - b))
+// console.log("Multiplication:" + (a * b))
+// console.log("Division:" + (a / b))
+// console.log("Modulus:" + (a % b))
+
+//PROGRAM 1
+let a=prompt("Enter a number:")
+if(a>0){
+    console.log("Positive number")
+}
+else if(a==0){
+    console.log("Zero")
+}
+else{
+    console.log("Negative Number")
+}
+
+//PROGRAM 2
+let b=prompt("Enter a Number:")
+if(b%2==0){
+    console.log("Even Number")
+}
+else{
+    console.log("Odd Number")
+}
+
+//PROGRAM 3
 let age=prompt("Enter your age:")
-let college=prompt("Enter your college name:")
-console.log("Name:" + name)
-console.log("Age:" + age)
-console.log("College:" + college)
-alert("Name:" + name)
-alert("Age:" + age)
-alert("College:" + college)
-let a=10
-let b=1
-console.log("Addition:" + (a + b))
-console.log("Subtraction:" + (a - b))
-console.log("Multiplication:" + (a * b))
-console.log("Division:" + (a / b))
-console.log("Modulus:" + (a % b))
+if (age>=18){
+    console.log("Eligible to vote")
+}
+else{
+    console.log("not eligible to vote")
+}
+
+//PROGRAM 4
+let marks=prompt("Enter your marks:")
+if (marks>=90){
+    console.log("Grade A")
+}
+else if(marks>=70){
+    console.log("Grade B")
+}
+else if (marks>=50){
+    console.log("Grade C")
+}
+else {
+    console.log("Fail")
+}
+
+//PROGRAM 5
+let num1=prompt("Enter first number:")
+let num2=prompt("Enter second number:")
+if(num1>num2){
+    console.log("Greater number is:" + num1)
+}
+else if(num1==num2){
+    console.log("Both numbers are equal")
+}
+else{
+    console.log("Greater number is:" + num2)
+}
