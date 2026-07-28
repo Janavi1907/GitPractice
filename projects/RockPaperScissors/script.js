7710b8067a28d0782666d6c1e1af23be28d7c8eb
Janavi S <janavimegu36@gmail.com>
2026-07-28 18:06:06 +0530

    Completed Rock Paper Scissors Game

--- projects/RockPaperScissors/script.js
+++ projects/RockPaperScissors/script.js
@@ -3,12 +3,67 @@ let paper = document.getElementById("paper")
 let scissors = document.getElementById("scissors")
 let choice = document.getElementById("choice")
 
-rock.addEventListener("click", function () {
+rock.addEventListener("click", function (e) {
     choice.textContent = "Your Choice: Rock"
 })
-paper.addEventListener("click", function () {
+paper.addEventListener("click", function (e) {
     choice.textContent = "Your Choice: Paper"
 })
-scissors.addEventListener("click", function () {
+scissors.addEventListener("click", function (e) {
     choice.textContent = "Your Choice: Scissors"
 })
+
+let choices=["Rock","Paper","Scissors"]
+function computerChoice() {
+    let randomChoice = Math.floor(Math.random() * choices.length)
+    return choices[randomChoice]
+}
+let compChoice = document.getElementById("compChoice")
+let buttons = document.querySelectorAll("button")
+let currentChoice=""
+buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+        currentChoice=computerChoice()
+        compChoice.textContent = "Computer Choice: " + currentChoice
+    })
+})
+
+let result=document.getElementById("result")
+buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+        let playerSelection = button.id.toLowerCase()
+        let computerSelection = currentChoice.toLowerCase()
+        if (playerSelection == computerSelection) {
+            result.textContent = "🤝 Result: It's a Draw!"
+            result.style.color="yellow"
+        } 
+        else if (playerSelection=="rock"){
+            if(computerSelection=="scissors"){
+                result.textContent="🎉Result: You Win!"
+                result.style.color="green"
+            }else{
+                result.textContent="❌ Result: You Lose!"
+                result.style.color="red"
+            }
+        }
+        else if (playerSelection=="paper"){
+            if(computerSelection=="rock"){
+                result.textContent="🎉 Result: You Win!"
+                result.style.color="green"
+            }else{
+                result.textContent="❌ Result: You Lose!"
+                result.style.color="red"
+            }
+        }
+        else if (playerSelection=="scissors"){
+            if(computerSelection=="paper"){
+                result.textContent="🎉 Result: You Win!"
+                result.style.color="green"
+            }else{
+                result.textContent="❌ Result: You Lose!"
+                result.style.color="red"
+            }
+        }
+    })           
+})
+
