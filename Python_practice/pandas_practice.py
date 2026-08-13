5b09a99cff524841728a05f4349869c6e3a796cc
Janavi S <janavimegu36@gmail.com>
2026-08-13 11:07:42 +0530

    Added IPL team statistics analysis

--- Python_practice/pandas_practice.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-data = {
-    "Team":["RCB","CSK","RCB","MI"],
-    "Wins":[10,8,7,9]
-}
-df=pd.DataFrame(data)
-print(df)
-print(df["Team"])
-print(df["Team"].value_counts())
--- projects/1ST YEAR PROJECTS/IPL-Statistics-Analysis/analysis/analysis.py
+++ projects/1ST YEAR PROJECTS/IPL-Statistics-Analysis/analysis/analysis.py
@@ -1,27 +1,22 @@
 # IPL Statistics Analysis
 # Project setup
 import pandas as pd
-df=pd.read_csv("projects/IPL-Statistics-Analysis/data/Match_Info.csv")
+df=pd.read_csv("projects/1ST YEAR PROJECTS/IPL-Statistics-Analysis/data/Match_Info.csv")
 winning_count=df["winner"].value_counts()
 print(winning_count)
 # PROGRAM 1
 most_wins=df["winner"].value_counts().index[0]
 print(most_wins)
-
 #PROGRAM 2
 least_wins=df["winner"].value_counts().index[-1]
 print(least_wins)
-
 #PROGRAM 3
 toss_match_win_same=df["toss_winner"]==df["winner"]
 print(toss_match_win_same.value_counts())
-
 #PROGRAM 4
 toss_match_win_same_count=toss_match_win_same.value_counts()[True]
 percentage=(toss_match_win_same_count/df.shape[0])*100
 print("Toss and Match win Same percentage",percentage.round(2),"%")
-
 # PROGRAM 5
 toss_decision=df["toss_decision"].value_counts().index[0]
 print("Most common toss decision:",toss_decision)
-
